179f6782fe3ddde2179429bcfad11a8137cc3dc1
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2012-10-31 17:00:48 +1100

    Auth helpers fixed

--- app/helpers/global_roles_helper.rb
+++ app/helpers/global_roles_helper.rb
@@ -1,11 +1,19 @@
 module GlobalRolesHelper
 
   def authorized_globaly_to?(controller, action)
-    User.current.global_permission_to?(controller, action)
+    if User.current.global_permission_to?(controller, action)
+      true
+    else
+       render_403
+    end
   end
 
   def authorized_globaly?
-    User.current.global_permission_to?(params[:controller], params[:action])
+    if User.current.global_permission_to?(params[:controller], params[:action])
+      true
+    else
+       render_403
+    end
   end   
 
 end
