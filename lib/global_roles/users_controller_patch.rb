3e9ecb7a57cbb4ef9ed6ed82e93f3a35b0cc7ca5
Alexey Glukhov <gluhov1985@gmail.com>
2014-03-20 13:43:10 +1100

    - Fixed bug where newly added global roles won't show jquery highlight effect in 'users/edit' and 'roles/edit' views

--- lib/global_roles/users_controller_patch.rb
+++ lib/global_roles/users_controller_patch.rb
@@ -25,11 +25,13 @@ module GlobalRoles
 
       def create_global_role
         @user = User.find(params[:id])
-        @roles = params[:roles]
+        @role_ids = params[:roles]
 
-        if @roles.is_a?(Array)
-          @roles.each{ |role_id| GlobalRole.create(:user_id => @user.id, :role_id => role_id ) }
+        if @role_ids.is_a?(Array)
+          @role_ids.each { |role_id| GlobalRole.create(:user_id => @user.id, :role_id => role_id ) }
         end
+
+        @global_role_ids = GlobalRole.select(:id).where(:user_id => @user.id, :role_id => @role_ids).map(&:id)
 
         respond_to do |format|
           format.html { redirect_to :controller => 'users', :action => 'edit', :tab => 'users-global-roles' }
