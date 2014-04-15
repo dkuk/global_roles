b493f2be22cda6cbd2a171023043e3b2f4b94315
Alexey Glukhov <gluhov1985@gmail.com>
2014-04-15 18:10:46 +1100

    #14438 Minor changes

--- lib/global_roles/roles_controller_patch.rb
+++ lib/global_roles/roles_controller_patch.rb
@@ -80,7 +80,7 @@ module GlobalRoles
 
         if @principal_ids.is_a?(Array)
           @principal_ids.each do |principal_id|
-            member = Member.find_or_new(params[:project_id], principal_id.to_i)
+            member = Member.first_or_initialize(params[:project_id], principal_id.to_i)
             if !member.roles.include?(@role)
               member.roles << @role
               @edited_principal_ids << principal_id
@@ -89,7 +89,6 @@ module GlobalRoles
 
           end
         end
-        logger.debug("@edited_principal_ids=#{@edited_principal_ids}")
 
         respond_to do |format|
           format.html { redirect_to :controller => 'roles', :action => 'edit', :tab => 'users_by_role'}
@@ -112,7 +111,7 @@ module GlobalRoles
             member_roles.destroy_all
 
             project_ids.each do |project_id|
-              member = Member.find_or_new(project_id, @principal_id)
+              member = Member.first_or_initialize(project_id, @principal_id)
               member.member_roles << MemberRole.new(:role_id => @role.id, :member_id => member.id)
               member.save
             end
